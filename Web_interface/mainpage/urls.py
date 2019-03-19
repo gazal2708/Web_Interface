ba75ea0f482ed97d4d49eee667e48714ff849a8d
Gazal Jain <33591515+gazal2708@users.noreply.github.com>
2019-03-19 20:22:56 +0530

    Update urls.py

--- Web_interface/mainpage/urls.py
+++ Web_interface/mainpage/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.home, name='home'),
     path('success/', views.afterValidateSuccess, name = "afterValidateSuccessUrl"),
     path('stopConfirmation/<int:item_id>', views.stopRunningConfirmation, name = "stopRunningConfirmationUrl"),
     path('stop/<int:item_id>', views.stopRunning, name = "stopRunningUrl")
